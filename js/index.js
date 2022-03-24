884f05a1144f4d6725b3cabcb73f7ff4413e2e8a
Suniel <sunieljolly@googlemail.com>
2022-03-24 12:37:04 +0700

    24-03-2022

--- js/index.js
+++ js/index.js
@@ -382,3 +382,14 @@ function leagueLoader() {
     }
   }
 }
+
+
+if("serviceWorker" in navigator){
+navigator.serviceWorker.register("sw.js").then(registration => {
+  console.log("SW Registered!");
+  console.log(registration);
+}).catch(error => {
+  console.log("SW Registered Failed!");
+  console.log(error);
+});
+}
